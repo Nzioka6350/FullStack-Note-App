ed8e89ace11f4dc49b8d5a0ef89a304ebcb3e27f
Nzioka6350 <jamesnziokamutua1@gmail.com>
2025-01-09 08:29:02 +0300

    Server

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,9 @@ const app = express();
 const port = 5000;
 app.set('view engine','ejs');
 
+app.get('/',(req,res)=>{
+    res.send("Yoh I'm just kidding")
+})
 
 app.listen(port,()=>{
     console.log("app is running on port",{port})
