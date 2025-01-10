8fe694139dce4a83feb23ba3685f5cf222d3eef4
Nzioka6350 <jamesnziokamutua1@gmail.com>
2025-01-10 14:19:19 +0300

    Server

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 const app = express();
 const port = 5000;
 app.set('view engine','ejs');
-
+app.use(express.static('public'))
 
 app.get('/test',(req,res)=>{
     res.send("Yoh I'm just kidding")
