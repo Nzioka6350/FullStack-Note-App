5c76d065bef2df993261fa1449b4d903ced1511e
Nzioka6350 <jamesnziokamutua1@gmail.com>
2025-01-15 15:24:09 +0300

    More

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 5000;
 app.set('view engine','ejs');
 app.use(express.static('public'))
+
 //To get login page
 app.get('/login',(req,res)=>{
     res.render('login')
